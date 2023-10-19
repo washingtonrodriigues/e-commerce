d5b6dfb75e8b96baf8f1829bda0fa9bb201c5058
Washington <washington.rodrigues@redgtech.com.br>
2023-10-19 02:32:40 -0300

    feat: ensure user goest to catagory page on categoryItem click

--- src/app/(home)/components/categorie-item.tsx
+++ src/app/(home)/components/categorie-item.tsx
@@ -3,6 +3,7 @@ import { Category } from "@prisma/client";
 import styles from "../../../styles/fonts.module.css";
 import classNames from "classnames";
 import { CATEGORY_ICON } from "@/constants/category-item";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -10,15 +11,17 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <Badge
-      variant="outline"
-      className="flex items-center justify-center gap-2 rounded-lg py-3"
-    >
-      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-      <span className={`text-xs font-bold ${styles.poppins}`}>
-        {category.name}
-      </span>
-    </Badge>
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex items-center justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className={`text-xs font-bold ${styles.poppins}`}>
+          {category.name}
+        </span>
+      </Badge>
+    </Link>
   );
 };
 
