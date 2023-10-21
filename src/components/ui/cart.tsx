f07bb48b8496e3667dca800c96aa3ac84f07be77
Washington <washington.rodrigues@redgtech.com.br>
2023-10-20 22:24:23 -0300

    feat: add payment checkout with stripe when user add a product on cart and click in finish purchase button

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -7,9 +7,22 @@ import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const { products, total, subtotal, totalDiscount } = useContext(CartContext);
+
+  const handleFinishPurchaseClick = async () => {
+    const checkout = await createCheckout(products);
+
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    stripe?.redirectToCheckout({
+      sessionId: checkout.id,
+    });
+  };
+
   return (
     <div className="flex h-full flex-col gap-8">
       <Badge
@@ -56,7 +69,12 @@ const Cart = () => {
           <p>Total</p>
           <p>R$ {total.toFixed(2).replace(".", ",")}</p>
         </div>
-        <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
+        <Button
+          onClick={handleFinishPurchaseClick}
+          className="mt-7 font-bold uppercase"
+        >
+          Finalizar compra
+        </Button>
       </div>
     </div>
   );
