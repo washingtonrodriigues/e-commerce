eecd2b2f4fb68585349dce2a5fdfb279ee9f19d1
Washington <washington.rodrigues@redgtech.com.br>
2023-10-26 11:10:48 -0300

    fix: ensure ProductItem photo always is square and simplify classes

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -13,45 +13,45 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <Link href={`/product/${product.slug}`}>
-      <div className={`poppins flex flex-col gap-4 ${styles.poppins}`}>
-        <div className="relative  flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
-          <Image
-            src={product.imageUrls[0]}
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto max-h-[70%] w-auto max-w-[80%]"
-            style={{ objectFit: "contain" }}
-            alt={product.name}
-          />
-          {product.discountPercent > 0 && (
-            <DiscountBadge className="absolute left-3 top-3">
-              {product.discountPercent}
-            </DiscountBadge>
-          )}
-        </div>
+    <Link
+      href={`/product/${product.slug}`}
+      className={`poppins flex min-w-[156px] flex-col gap-4 ${styles.poppins}`}
+    >
+      <div className="relative flex  aspect-square h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+          alt={product.name}
+        />
+        {product.discountPercent > 0 && (
+          <DiscountBadge className="absolute left-3 top-3">
+            {product.discountPercent}
+          </DiscountBadge>
+        )}
+      </div>
 
-        <div className="flex flex-col gap-1">
-          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-            {product.name}
-          </p>
-          <div className="flex items-center gap-2 whitespace-nowrap">
-            {product.discountPercent > 0 ? (
-              <>
-                <p className="font-semibold">
-                  R$ {product.totalPrice.toFixed(2).replace(".", ",")}
-                </p>
-                <p className="text-xs line-through opacity-75 ">
-                  R$ {Number(product.basePrice).toFixed(2).replace(".", ",")}
-                </p>
-              </>
-            ) : (
-              <p className="text-sm font-semibold">
-                R$ {product.basePrice.toFixed(2).replace(".", ",")}
+      <div className="flex flex-col gap-1">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          {product.name}
+        </p>
+        <div className="flex items-center gap-2 whitespace-nowrap">
+          {product.discountPercent > 0 ? (
+            <>
+              <p className="font-semibold">
+                R$ {product.totalPrice.toFixed(2).replace(".", ",")}
               </p>
-            )}
-          </div>
+              <p className="text-xs line-through opacity-75 ">
+                R$ {Number(product.basePrice).toFixed(2).replace(".", ",")}
+              </p>
+            </>
+          ) : (
+            <p className="text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2).replace(".", ",")}
+            </p>
+          )}
         </div>
       </div>
     </Link>
