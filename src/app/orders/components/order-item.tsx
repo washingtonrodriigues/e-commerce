1ac24a878d53f0d0c0914e57a58b33c24e6e4f06
Washington <washington.rodrigues@redgtech.com.br>
2023-10-28 21:22:53 -0300

    style: change order-item text size

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -47,7 +47,7 @@ const OrderItem = ({ order }: OrderItemProps) => {
         <AccordionItem value={order.id}>
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
-              <p className="font-bold uppercase">
+              <p className="text-sm font-bold uppercase">
                 Pedido com {order.orderProducts.length} produto(s)
               </p>
               <span className="text-xs opacity-60">
