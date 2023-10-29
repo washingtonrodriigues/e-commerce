1ca16d193c963d6850b885663996afd6551d7cf7
Washington <washington.rodrigues@redgtech.com.br>
2023-10-29 01:15:02 -0300

    feat: add search input to home page but it's not working yet

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "../../components/ui/product-list";
 import Sectiontitle from "@/components/ui/section-title";
 import PromoBanner from "./components/promo-banner";
 import Link from "next/link";
+import { SearchIcon } from "lucide-react";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -32,6 +32,18 @@ export default async function Home() {
   });
   return (
     <div className="flex flex-col gap-8 py-8">
+      {/* <div className="m-auto flex w-[90%] border-[1px] border-solid border-[#969696] py-2"> */}
+      <div className="relative m-auto flex w-[90%] items-center justify-between rounded-lg border-[1px] border-solid border-[#949494] bg-accent p-2 transition-[.3s] placeholder:text-[#6e6e6e] focus-within:ring-2 focus-within:ring-[#6847ed]">
+        <input
+          type="search"
+          placeholder="Pesquisar"
+          name=""
+          id=""
+          className="w-full bg-transparent outline-none placeholder:text-[#949494]"
+        />
+        <button>{<SearchIcon size={20} color="#949494" />}</button>
+      </div>
+
       <PromoBanner
         src="/banner_home_01.png"
         alt="até 55% de desconto só esse mês!"
